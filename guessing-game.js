9ae2cdb55be5da0b4c26a69fa4934599a2ddb122
Agnes <agnes.mccall5@gmail.com>
2024-11-02 21:06:58 -0400

    add function guessingGame

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,21 @@
 function guessingGame() {
+    let randomNumber = Math.floor(Math.random() * 100);
+    let guesses = 0;
+    let win = false;
+    return function guess(num) {
+        guesses++;
 
+        if (win) {
+            return "The game is over, you already won!"
+        } else if (num < randomNumber) {
+            return `${num} is too low!`;
+        } else if (num > randomNumber) {
+            return `${num} is too high!`;
+        } else {
+            win = true;
+            return `You win! You found ${num} in ${guesses} guesses.`;
+        }
+    }
 }
 
 module.exports = { guessingGame };
